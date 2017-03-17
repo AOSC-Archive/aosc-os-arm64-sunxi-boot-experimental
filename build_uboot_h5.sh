f2c747a6e4abb6a07d6996217c959fc03b96b769
Icenowy Zheng <icenowy@aosc.xyz>
2017-03-17 19:02:22 +0800

    disable ATF debug info
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- build_uboot_h5.sh
+++ build_uboot_h5.sh
@@ -4,7 +4,7 @@ cd uboot-h5
 git clone https://github.com/AOSC-Dev/u-boot-aosc-sunxi u-boot -b aosc-h5 --depth=1
 git clone https://github.com/Icenowy/arm-trusted-firmware -b allwinner --depth=1
 pushd arm-trusted-firmware
-make PLAT=sun50iw1p1 CROSS_COMPILE=/opt/abcross/arm64/bin/aarch64-aosc-linux-gnu- DEBUG=1
+make PLAT=sun50iw1p1 CROSS_COMPILE=/opt/abcross/arm64/bin/aarch64-aosc-linux-gnu-
 cp build/sun50iw1p1/debug/bl31.bin ../u-boot/
 popd
 pushd u-boot
