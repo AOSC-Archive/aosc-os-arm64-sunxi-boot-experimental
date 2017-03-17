71729a7ab8483009f47854b06dae617e1484cb33
Icenowy Zheng <icenowy@aosc.xyz>
2017-03-17 19:07:26 +0800

    fix ATF finding
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- build_uboot_a64.sh
+++ build_uboot_a64.sh
@@ -5,7 +5,7 @@ git clone https://github.com/AOSC-Dev/u-boot-aosc-sunxi u-boot -b aosc-a64 --dep
 git clone https://github.com/Icenowy/arm-trusted-firmware -b allwinner --depth=1
 pushd arm-trusted-firmware
 make PLAT=sun50iw1p1 CROSS_COMPILE=/opt/abcross/arm64/bin/aarch64-aosc-linux-gnu-
-cp build/sun50iw1p1/debug/bl31.bin ../u-boot/
+cp build/sun50iw1p1/release/bl31.bin ../u-boot/
 popd
 pushd u-boot
 make pine64_plus_defconfig
--- build_uboot_h5.sh
+++ build_uboot_h5.sh
@@ -5,7 +5,7 @@ git clone https://github.com/AOSC-Dev/u-boot-aosc-sunxi u-boot -b aosc-h5 --dept
 git clone https://github.com/Icenowy/arm-trusted-firmware -b allwinner --depth=1
 pushd arm-trusted-firmware
 make PLAT=sun50iw1p1 CROSS_COMPILE=/opt/abcross/arm64/bin/aarch64-aosc-linux-gnu-
-cp build/sun50iw1p1/debug/bl31.bin ../u-boot/
+cp build/sun50iw1p1/release/bl31.bin ../u-boot/
 popd
 pushd u-boot
 make sun50i_h5_spl32_defconfig
